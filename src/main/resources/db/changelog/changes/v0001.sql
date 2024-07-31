6c95a1c77cd1d0ad8a3a16e43a003ad19691a65f
NikitaDakuko <nikita.dakuko@gmail.com>
2024-07-31 16:43:59 +0300

    Docker compose, Liquibase

--- src/main/resources/db/changelog/changes/v0001.sql
+++ src/main/resources/db/changelog/changes/v0001.sql
@@ -1,8 +1,43 @@
-CREATE TABLE landmark (
+--changeset nikita:1
+DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS landmark;
+DROP TABLE IF EXISTS service;
+DROP TABLE IF EXISTS landmark_service;
+
+CREATE TABLE IF NOT EXISTS location (
     id SERIAL NOT NULL,
     name VARCHAR(50) NOT NULL,
-    creationdate DATE NOT NULL,
+    population BIGINT NOT NULL,
     description VARCHAR(255) NOT NULL,
-    type VARCHAR(25) NOT NULL,
+    metro BOOLEAN NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS landmark (
+    id SERIAL NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    creation_date DATE NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    location_id BIGINT NOT NULL,
+    type VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT location_landmark
+        FOREIGN KEY(location_id)
+            REFERENCES location(id)
+);
+
+CREATE TABLE IF NOT EXISTS service (
+    id SERIAL NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS landmark_service (
+    landmark_id BIGINT REFERENCES landmark(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    service_id BIGINT REFERENCES service(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
